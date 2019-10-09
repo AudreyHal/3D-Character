bc7adbf8c18be312dc6ce74cedcdc82257d9287c
AudreyHal <chiomahalim@gmail.com>
2019-10-09 04:59:03 -0700

    Resize Window

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -51,3 +51,9 @@ function animate() {
     if ( mixer ) mixer.update( delta );
     renderer.render( scene, camera );
 }
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+}

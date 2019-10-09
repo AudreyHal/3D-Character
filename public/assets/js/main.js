234d9204114028eb09e2cf60b6806f5d5d4b6137
AudreyHal <chiomahalim@gmail.com>
2019-10-09 04:56:03 -0700

    Add Lighting

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -9,5 +9,17 @@ function init() {
     scene = new THREE.Scene();
     scene.background = new THREE.Color( 0xa0a0a0 );
     scene.fog = new THREE.Fog( 0xa0a0a0, 200, 1000 );
+    light = new THREE.HemisphereLight( 0xffffff, 0x444444 );
+    light.position.set( 0, 200, 0 );
+    scene.add( light );
+    light = new THREE.DirectionalLight( 0xffffff );
+    light.position.set( 0, 200, 100 );
+    light.castShadow = true;
+    light.shadow.camera.top = 180;
+    light.shadow.camera.bottom = - 100;
+    light.shadow.camera.left = - 120;
+    light.shadow.camera.right = 120;
+    scene.add( light );
+    
 }
 function animate() {}

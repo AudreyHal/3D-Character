f0a170b344fc0d487098a4e3d564d83da4fa35eb
AudreyHal <chiomahalim@gmail.com>
2019-10-09 04:53:41 -0700

    Setup camera and scene

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,5 +1,13 @@
 var container, controls, camera, scene, renderer, light;
 init();
 animate();
-function init() {}
+function init() {
+    container = document.createElement( 'div' );
+    document.body.appendChild( container );
+    camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 2000 );
+    camera.position.set( 100, 200, 300 );
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color( 0xa0a0a0 );
+    scene.fog = new THREE.Fog( 0xa0a0a0, 200, 1000 );
+}
 function animate() {}
